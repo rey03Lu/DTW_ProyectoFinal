25c8be78616a0a23a5f6a568c42ba6c17ba3216c
Javier Peraza <contacto.peraza@gmail.com>
2025-05-04 17:30:00 -0600

    Se crea el modelo  provisional para hacer pruebas
     			$table->id();
                $table->string('title');
                $table->text('description')->nullable();
                $table->enum('status', ['pending', 'in_progress', 'completed'])->default('pending');
                $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Relación con usuarios
                $table->timestamps();

---
+++ app/Models/Task.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('status', ['pending', 'in_progress', 'completed'])->default('pending');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Relación con usuarios
+            $table->timestamps();
+        });
+    }
+}
